c03031345c23496a0e024f3bc7c68239e0480129
Will <korea651@hanmail.net>
2023-06-05 16:48:51 +0900

    update script

--- src/processor.ts
+++ src/processor.ts
@@ -7,14 +7,14 @@ import {In} from "typeorm"
 import * as rmrk from "./abi/rmrk"
 import {Owner, Token, Contract, ContractToken, OwnerContractToken, Transfer} from "./model/generated"
  
-const BASE_CONTRACT_ADDRESSES = ['0x5a472c3f3768fd7388b79b50358a4586d069e52ed8d4afdfb97e9b9a17858aa0', '0xab9b527849d82fd8ad0ca796fa6379e1168f680f7c8e0dfcd0be560866fb79f5']
-const BASE_CONTRACT_SS58_ADDRESSES = ['XyfkCVRSRTChUft42e8c6FD7RhTeAuPcq8sNZ8vb1PbycDB', 'ZpJd27qjg3VNk5gTSeMSVii8p4VLMBjy67mBvWDQWRRsZzm']
+const BASE_CONTRACT_ADDRESSES = ['0x045c301440e028ca69374ae856650032413b41917b982813ec4695d3f47d41cd']
+const BASE_CONTRACT_SS58_ADDRESSES = ['W31sRs7oHgzYTLa2xoVR8yU6dXC3GnUBBMQo2HoCY4Fneyq']
 
-const EQUIP_CONTRACT_ADDRESSES = ['0xcf61acd6296b717b8f18b49e2e4390058490ea13858d71c502e8a9cd1cf38d81', '0x0c8ba69231ceffb9b6dca2bf77d3a47011fb0c100a0bb0730d566d7efab9d931', '0x3e707397a9159b2214da3d36346ead6f053aa3ef30cf277630149c910f343efe', '0xefea7e8dc7dcfc12554d93b18063791c47653415e90b4a622b3133e3198ed1e8']
+// const EQUIP_CONTRACT_ADDRESSES = ['0xcf61acd6296b717b8f18b49e2e4390058490ea13858d71c502e8a9cd1cf38d81', '0x0c8ba69231ceffb9b6dca2bf77d3a47011fb0c100a0bb0730d566d7efab9d931', '0x3e707397a9159b2214da3d36346ead6f053aa3ef30cf277630149c910f343efe', '0xefea7e8dc7dcfc12554d93b18063791c47653415e90b4a622b3133e3198ed1e8']
 // const EQUIP_CONTRACT_SS58_ADDRESSES = ['adDDmXkrVUhcFNy74zJm9CohrvDCbBixhvLCzrrmzo5HG3U', 'WDkMQy5AgSXfByPW23szAFYrxtXbNUA7umL49YbTZysfa9w', 'XMAfed8ZvqDUQzoy8NuU715vhKs6rRckSYwSWuqQsM8ZWGv', 'bMsPywwU4m9F9ZZ48cSffEyGeYJ66A5N3PGDv76Yogpp7zX']
  
 const processor = new SubstrateBatchProcessor()
-    .setBlockRange({ from: 3397990 })
+    .setBlockRange({ from: 3890990 })
     .setDataSource({
         archive: lookupArchive("shibuya", { release: "FireSquid" })
     })
@@ -33,13 +33,13 @@ for (let i = 1; i < BASE_CONTRACT_ADDRESSES.length; i++) {
   } as const)
 }
 
-for (const address of EQUIP_CONTRACT_ADDRESSES) {
-  processor.addContractsContractEmitted(address, {
-    data: {
-        event: {args: true}
-    }
-  } as const)
-}
+// for (const address of EQUIP_CONTRACT_ADDRESSES) {
+//   processor.addContractsContractEmitted(address, {
+//     data: {
+//         event: {args: true}
+//     }
+//   } as const)
+// }
  
 type Item = BatchProcessorItem<typeof processor>
 type Ctx = BatchContext<Store, Item>
@@ -186,7 +186,7 @@ function extractRecords(ctx: Ctx): TransferRecord[] {
         for (const item of block.items) {
           // ctx.log.info('Item: ' + item.name)
           // ctx.log.info(item)
-            if (item.name === 'Contracts.ContractEmitted' && (BASE_CONTRACT_ADDRESSES.includes(item.event.args.contract) || EQUIP_CONTRACT_ADDRESSES.includes(item.event.args.contract))) {
+            if (item.name === 'Contracts.ContractEmitted' && (BASE_CONTRACT_ADDRESSES.includes(item.event.args.contract))) { //|| EQUIP_CONTRACT_ADDRESSES.includes(item.event.args.contract))) {
                 const event = rmrk.decodeEvent(item.event.args.data)
                 ctx.log.info(event)
                 if (event.__kind === 'Transfer') {
